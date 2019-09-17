843efa017c53e55f70ce50ffd93c43a3c34eefdd
Christopher DiStasio <cadistasio@gmail.com>
2019-09-16 22:07:45 -0500

    Creates, stubs Journal

---
+++ src/components/Journal.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Journal extends Component {
+  render(){
+    return (
+      <div>
+        <h2>Your Journal</h2>
+        <br></br>
+          {/* Input component */}
+        <br></br>
+        <div>
+          {/* Returns all entries, including newest one */}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Journal;

99cc3581b942280024d2636f054c98f7f2bc225b
Christopher DiStasio <cadistasio@gmail.com>
2019-09-16 21:19:33 -0500

    Adds store to index.js; creates, moves App to components folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+let store = createStore(manageJournal);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);

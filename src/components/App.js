0f4e4acd08d744464dbc8eb683d9852044f78888
Christopher DiStasio <cadistasio@gmail.com>
2019-09-16 21:45:21 -0500

    Adds Navbar, Journal routes to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import '../App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import NavBar from './NavBar.js'
+import Journal from './Journal.js'
+
 
 function App() {
   return (
-    "This component is being rendered."
+    <Router>
+      <React.Fragment>
+      <NavBar />
+        {
+          <Route 
+            exact path='/journal' 
+            component={Journal}
+          />
+        }
+        <div className="App">
+        </div>
+        </React.Fragment>
+    </Router>
   );
 }
 

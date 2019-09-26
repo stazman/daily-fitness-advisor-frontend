dff56de4c75f0a62ee8ee36765973d0d8d20046a
Christopher DiStasio <cadistasio@gmail.com>
2019-09-26 11:16:03 -0500

    Adds working links to views of new journal entry form, all journal entries

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -1,16 +1,21 @@
 import React from 'react'
-import JournalContainer from '../containers/JournalContainer'
-
+import { NavLink } from 'react-router-dom'
+ 
 const Journal = () => {
   return (
     <div>
-      <h2>
-        Your Journal
-      </h2>
-      <br></br>
-      <JournalContainer />
+      <h3>
+        Your Weight Loss Journal
+      </h3>
+      <NavLink exact to={'/journals/new'}>
+        Add a New Entry
+      </NavLink>
+      <br></br><br></br>
+      <NavLink exact to={'/journals'}>
+        View All Entries
+      </NavLink>
     </div>
   )
 }
-
+ 
 export default Journal;

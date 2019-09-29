0b8b0698b70ae3e3da5bb7e8292c8fdce3958e50
Christopher DiStasio <cadistasio@gmail.com>
2019-09-28 19:28:49 -0500

    Renders all journal entries most recent entry first; each link to an entry renders cohtent, route correctly

--- src/components/AllJournalEntries.js
+++ src/components/AllJournalEntries.js
@@ -3,19 +3,19 @@ import {Link} from 'react-router-dom'
 
 const AllJournalEntries = (props) => {
 
-    return (
-      <div>
-        { 
-          props.journalEntries.map(journalEntry => 
-            <div key={journalEntry.id}> 
-              <Link to={`/journals/${journalEntry.id}`}>
-                {journalEntry.content}
-              </Link>
-            </div>
-          )
-        }                
-      </div>
-    )
-  }
+  return (
+    <div>
+      { 
+        [...props.journalEntries].reverse().map(journalEntry => 
+          <div key={journalEntry.id}> 
+            <Link to={`/journals/${journalEntry.id}`}>
+              {journalEntry.content}
+            </Link>
+          </div>
+        )
+      }                
+    </div>
+  )
+}
 
 export default AllJournalEntries;

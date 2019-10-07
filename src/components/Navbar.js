dde113e5105df8085bd2ba1ff8261774732dfb73
Christopher DiStasio <cadistasio@gmail.com>
2019-10-07 10:35:22 -0500

    Styles Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,20 +5,37 @@ import { NavDropdown } from 'react-bootstrap'
 
 const NavBar = () => {
   return (
-    <div className='navbar'>
-      <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">Daily Weight Loss Advisor</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <NavDropdown id="basic-nav-dropdown">
-            <NavDropdown.Item href="/">Settings</NavDropdown.Item>
-            <NavDropdown.Item href="/">Login/Logout</NavDropdown.Item>
+    <>
+    <style type="text/css">
+      {`
+        .nb-bg {
+          background-color: navy;
+        }
+        .navbar-light .navbar-brand,
+        .navbar-light .navbar-nav .nav-link
+         {
+          color: white;
+        }
+      `}
+    </style>
+
+    <div className='nb-bg'>
+      <Navbar expand='lg' className='nb-bg'>
+      <Navbar.Brand href='/'>Daily Weight Loss Advisor</Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
+          <NavDropdown id='basic-nav-dropdown'>
+            <NavDropdown.Item href='/'>About DWLA </NavDropdown.Item>
+            <NavDropdown.Item href='/'>Contact DWLA</NavDropdown.Item>
+            <NavDropdown.Item href='/'>Settings</NavDropdown.Item>
+            <NavDropdown.Item href='/'>Login/Logout</NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
       </Navbar>
     </div>
+    </>
   )
 }
 

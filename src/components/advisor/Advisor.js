4709f416ad82eb0bc2d9ca500aadf8547bd23d31
Christopher DiStasio <cadistasio@gmail.com>
2019-10-07 11:32:20 -0500

    Stubs content; adds layout components, elements; adds, styles new advice button on home page

--- src/components/advisor/Advisor.js
+++ src/components/advisor/Advisor.js
@@ -1,6 +1,35 @@
+import React from 'react'
+import { Container, Col, Row, Button } from 'react-bootstrap'
+
 const Advisor = () => {
   return (
-    "This is the future Advisor/home page. It will render the form/s and resulting output for daily advice. "
+    <>
+    <style type="text/css">
+      {`
+        .btn-main {
+          background-color: navy;
+          color: white;
+        }
+      `}
+    </style>
+
+    <Container className='container'>
+      <Row>
+        <Col>
+          <div>
+            Losing weight is a marathon, not a sprint. But even a marathon begins with one step ...
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant="main" href='/advisors/new'>
+            Get Advice
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+    </>
   )
 }
 

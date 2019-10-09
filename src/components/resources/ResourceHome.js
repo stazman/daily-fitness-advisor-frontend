c3415cf01fb22a6ea5b53776d07fb9f63de4f1f0
Christopher DiStasio <cadistasio@gmail.com>
2019-10-08 23:22:17 -0500

    Stubs styling for resource home page

--- src/components/resources/ResourceHome.js
+++ src/components/resources/ResourceHome.js
@@ -1,20 +1,56 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Container, Col, Row, Button } from 'react-bootstrap'
  
 const ResourceHome = () => {
   return (
-    <div>
-      <h3>
-        Your Resources
-      </h3>
-      <NavLink exact to={'/resources/new'}>
-        Add a New Resource
-      </NavLink>
-      <br></br><br></br><br></br>
-      <NavLink exact to={'/resources'}>
-        View All Resources
-      </NavLink>
-    </div>
+    <>
+    <style type="text/css">
+      {`
+        .flex-container {
+          display: flex;
+          width: 100vw;
+          height: 100vh;
+          flex-direction: column;
+          align-items: center;
+        }
+
+        .btn-main {
+          background-color: navy;
+          color: white;
+        }
+      `}
+    </style>
+
+    <Container className='flex-container'>
+      <Row>
+        <Col lg={12}>
+          <h4>
+            Your Resources
+          </h4>
+        </Col>
+      </Row>
+    </Container>
+
+    <Container className='flex-container'>
+      <Row>
+        <Col lg={12}>
+          <Button variant="main" href='/resources/new'>
+            Add a New Resource
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+
+    <Container className='flex-container'>
+      <Row>
+        <Col lg={12}>
+          <Button variant="main" href='/resources'>
+            View All Resources
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+    </>
   )
 }
  

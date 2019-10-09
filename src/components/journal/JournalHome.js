eefaa8352fa91cacfa20c921152f65e2e32df07b
Christopher DiStasio <cadistasio@gmail.com>
2019-10-08 23:20:56 -0500

    Stubs styling for journal home page

--- src/components/journal/JournalHome.js
+++ src/components/journal/JournalHome.js
@@ -1,20 +1,56 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
- 
+import { Container, Col, Row, Button } from 'react-bootstrap'
+
 const JournalHome = () => {
   return (
-    <div>
-      <h3>
-        Your Weight Loss Journal
-      </h3>
-      <NavLink exact to={'/journals/new'}>
-        Add a New Entry
-      </NavLink>
-      <br></br><br></br>
-      <NavLink exact to={'/journals'}>
-        View All Entries
-      </NavLink>
-    </div>
+    <>
+    <style type="text/css">
+      {`
+        .flex-container {
+          display: flex;
+          width: 100vw;
+          height: 100vh;
+          flex-direction: column;
+          align-items: center;
+        }
+
+        .btn-main {
+          background-color: navy;
+          color: white;
+        }
+      `}
+    </style>
+
+    <Container className='flex-container'>
+      <Row>
+        <Col lg={12}>
+          <h4>
+            Your Weight Loss Journal
+          </h4>
+        </Col>
+      </Row>
+    </Container>
+
+    <Container className='flex-container'>
+      <Row>
+        <Col lg={12}>
+          <Button variant="main" href='/journals/new'>
+            Add a New Entry
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+
+    <Container className='flex-container'>
+      <Row>
+        <Col lg={12}>
+          <Button variant="main" href='/journals'>
+            View All Entries
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+    </>
   )
 }
  

813c32399c034c3c4cd5ab89c69042b0c98ac69f
Christopher DiStasio <cadistasio@gmail.com>
2019-10-10 20:39:39 -0500

    Replaces stubbed styling code in BottomNavbar with NavbarStyles

--- src/components/BottomNavbar.js
+++ src/components/BottomNavbar.js
@@ -1,34 +1,18 @@
 import React from 'react'
-import { Container, Col, Row, Navbar } from 'react-bootstrap'
+import { Navbar } from 'react-bootstrap'
+import NavbarStyles from '../components/styles/NavbarStyles'
 
 const BottomNavBar = () => {
   return (
     <>
-    <style type="text/css">
-      {`
-        .nb-bg {
-          background-color: navy;
-        }
-        .navbar-light .navbar-brand,
-        .navbar-light .navbar-nav .nav-link
-         {
-          color: white;
-        }
-      `}
-    </style>
-
-    <Container className='flex-container'>
-      <Row>
-        <Col lg={12}>
-          <Navbar expand='lg' className='nb-bg'>
-            <Navbar.Brand href='/tracker_home'>Trackers</Navbar.Brand>
-            <Navbar.Brand href='/journal_home'>Journal</Navbar.Brand>
-            <Navbar.Brand href='/resource_home'>Resources</Navbar.Brand>
-            <Navbar.Brand href='/community_home'>Community</Navbar.Brand>
-          </Navbar>
-        </Col>
-      </Row>
-    </Container>
+    <NavbarStyles>
+      <Navbar expand='xl' className='nb-bg'>
+        <Navbar.Brand href='/tracker_home'>Trackers</Navbar.Brand>
+        <Navbar.Brand href='/journal_home'>Journal</Navbar.Brand>
+        <Navbar.Brand href='/resource_home'>Resources</Navbar.Brand>
+        <Navbar.Brand href='/community_home'>Community</Navbar.Brand>
+      </Navbar>
+    </NavbarStyles>
     </>
   )
 }

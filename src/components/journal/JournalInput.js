201a94f8c431bb11f2555c1b9a661362b12e3e3d
Christopher DiStasio <cadistasio@gmail.com>
2019-10-11 17:56:40 -0500

    Stubs styling of new journal entry form

--- src/components/journal/JournalInput.js
+++ src/components/journal/JournalInput.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import { withRouter } from 'react-router-dom';
 import {addJournalEntry} from '../../actions/addJournalEntry'
-import {
-	withRouter
-} from 'react-router-dom';
-
+import { Form, Button } from 'react-bootstrap'
+import ButtonStyles from '../styles/ButtonStyles'
+import FormStyles from '../styles/FormStyles'
 
 class JournalInput extends Component {
 
@@ -29,19 +29,32 @@ class JournalInput extends Component {
 
   render(){
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>Add a Journal Entry:</label>
-          <br></br><br></br>
-          <input
-            type='textarea'
-            placeholder='New Entry'
-            onChange={e => this.handleChange(e)}
-            value={this.state.content}
-          />
-          <input type="submit" />
-        </form>
-      </div>
+      <FormStyles>
+        <ButtonStyles>
+
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group controlId='formJournalEntryl'>
+              <Form.Label>Add a Journal Entry</Form.Label>
+              <br></br>
+              <Form.Control 
+                as='textarea' 
+                type='textarea'
+                rows='6'
+                placeholder='Write your journal entry here ...'
+                onChange={e => this.handleChange(e)}
+                value={this.state.content}
+              />
+              <Form.Text>                
+                Keep note of your progress and thoughts!
+              </Form.Text>
+            </Form.Group>
+            <Button className='btn-main' type='submit'>
+              Submit
+            </Button>
+          </Form>
+          
+        </ButtonStyles>
+      </FormStyles>
     )
   }
 }

e28f37a50082bafa28fc20f2bafd0dfe211f84d2
Christopher DiStasio <cadistasio@gmail.com>
2019-10-10 20:34:56 -0500

    Creates, adds styling to, replaces stubbed styling code in TopNavbar with NavbarStyles

---
+++ src/components/styles/NavbarStyles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const NavbarStyles = styled.div`
+  .nb-bg {
+    background-color: navy;
+  }
+
+  .navbar-light .navbar-brand,
+  .navbar-light .navbar-nav .nav-link {
+    color: white;
+  }
+`
+export default NavbarStyles

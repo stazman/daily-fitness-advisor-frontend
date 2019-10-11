51df34cd007e9d879b8d980052d8c75e1497a077
Christopher DiStasio <cadistasio@gmail.com>
2019-10-10 19:42:11 -0500

    Adds Layout, BottomNavbar, Switch to App; removes unneeded code, css files

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
-import '../App.css'
-import {Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import TopNavbar from './TopNavbar'
 import Advisor from './advisor/Advisor'
 import TrackerHome from './trackers/TrackerHome'
@@ -10,77 +9,53 @@ import ResourceHome from './resources/ResourceHome'
 import ResourcesContainer from '../containers/ResourcesContainer'
 import CommunityHome from './community/CommunityHome'
 import BottomNavbar from './BottomNavbar'
-import { Container } from 'react-bootstrap'
+import { Layout } from './Layout'
 
 class App extends Component {
   render(){
     return (
       <>
-      <style type="text/css">
-        {`
-          .flex-container {
-            display: flex;
-            width: 100%;
-            height: 100%;
-            margin: 0;
-            padding: 0;
-            flex-direction: column;
-          }
-          .row {
-            margin: 0;
-            padding: 0;
-          }
-        `}
-      </style>
-
-      <Container className='flex-container'>
         <TopNavbar />
-        {
-          <Route 
-            exact path='/' 
-            component={Advisor}
-          />
-        }
-        {
-          <Route 
-            exact path='/tracker_home' 
-            component={TrackerHome}
-          />
-        }
-        {
-          <Route 
-            exact path='/journal_home' 
-            component={JournalHome}
-          />
-        }
-        {
-          <Route 
-            exact path='/resource_home' 
-            component={ResourceHome}
-            //(Rather than a button on the bottom of the screen, may go in a dropdown with login, settings links/buttons)
-          />
-        }
-        {
-          <Route 
-            exact path='/community_home' 
-            component={CommunityHome}
-          />
-        }
-        <JournalContainer />
-        <ResourcesContainer />
+        <Layout>
+          <Switch>
+            {
+              <Route 
+                exact path='/' 
+                component={Advisor}
+              />
+            }
+            {
+              <Route 
+                exact path='/tracker_home' 
+                component={TrackerHome}
+              />
+            }
+            {
+              <Route 
+                exact path='/journal_home' 
+                component={JournalHome}
+              />
+            }
+            {
+              <Route 
+                exact path='/resource_home' 
+                component={ResourceHome}
+              />
+            }
+            {
+              <Route 
+                exact path='/community_home' 
+                component={CommunityHome}
+              />
+            }
+          </Switch>
 
-        {/* AdvisorContainer
-        TrackerContainer
-          WeightLossTrackerContainer (Has form for new weighing, current weight, weight history)
-          HabitTracker (Shows Record of Habits for week, month, etc.)
-        ResourcesContainer
-          CommentsContainer
-        CommunityContainer
-          CommunityLinksContainer
-          CommentsContainer */}
+          <JournalContainer />
+          
+          <ResourcesContainer />
 
+        </Layout>
         <BottomNavbar />
-      </Container>
       </>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './components/App'
 import manageJournal from './reducers/manageJournal'
 import manageResources from './reducers/manageResources'

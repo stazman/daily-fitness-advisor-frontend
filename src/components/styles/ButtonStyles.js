61f98071646e2cf2c63694c06ba56834f4432dab
Christopher DiStasio <cadistasio@gmail.com>
2019-10-10 20:41:51 -0500

    Adds ButtonStyles

---
+++ src/components/styles/ButtonStyles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const ButtonStyles = styled.div`
+  .btn-main {
+    background-color: navy;
+    color: white;
+  }
+
+  .btn-submit {
+    font-size: .8em;
+    background-color: navy;
+    color: white;
+  }
+`
+
+export default ButtonStyles

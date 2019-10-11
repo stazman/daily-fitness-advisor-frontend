20f9d1a9b932448842a4abf73d6695dfccba6f5f
Christopher DiStasio <cadistasio@gmail.com>
2019-10-10 20:44:57 -0500

    Adds ColumnStyles

---
+++ src/components/styles/ColumnStyles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const ColumnStyles = styled.div`
+  .col-lg-12.message-col {
+    min-height: 10em;
+    padding-top: 5em;
+    text-align: center;
+  }
+
+  .col-lg-12.button-col {
+    min-height: 8em;
+    text-align: center;
+  }
+`
+
+export default ColumnStyles

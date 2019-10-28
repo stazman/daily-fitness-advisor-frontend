09e68d0dc2a0c7b136b59cfc15ba8384628ec347
Christopher DiStasio <cadistasio@gmail.com>
2019-10-28 14:27:53 -0500

    Creates, stubs container for food advisor

--- src/containers/FoodAdvisorsContainer.js
+++ src/containers/FoodAdvisorsContainer.js
@@ -1,28 +1,36 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
-// import { fetchFoodAdvisors } from '../actions/fetchFoodAdvisors'
+import { fetchLatestFoodAdvice } from '../actions/fetchLatestFoodAdvice'
 import FoodAdvisorInput from '../components/food_advisor/FoodAdvisorInput'
 
 class FoodAdvisorsContainer extends React.Component {
 
-  // componentDidMount(){
-  //   this.props.fetchFoodAdvisors()
-  // }
+  componentDidMount(){
+    this.props.fetchLatestFoodAdvice()
+  }
 
   render(){
     return (
       <div>
-        {/* <Switch> */}
+        <Switch>
+
           <Route
             exact path='/food_advisors/new'
             component={FoodAdvisorInput}
           />
+
           {/* <Route
             exact path='/food_advisors/'
             render={(routerProps) => <FoodAdvisors {...routerProps} resources={this.props.foodAdvisors} />}
           /> */}
-        {/* </Switch> */}
+
+          {/* <Route
+            exact path='/food_advisors/'
+            render={(routerProps) => <FoodAdvisors {...routerProps} resources={this.props.foodAdvisors} />}
+          /> */}
+
+        </Switch>
       </div>
     )
   }
@@ -30,8 +38,8 @@ class FoodAdvisorsContainer extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    foodAdvisors: state.manageFoodAdvisors.foodAdvisors
+    foodAdvisors: state.manageFoodAdvice.foodAdvisors
   }
 }
 
-export default connect(mapStateToProps, {fetchFoodAdvisors})(FoodAdvisorsContainer)
+export default connect(mapStateToProps, {fetchLatestFoodAdvice})(FoodAdvisorsContainer)

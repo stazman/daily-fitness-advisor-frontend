57169507eaa4d36f5d89ea20e2f2586f83020285
Christopher DiStasio <cadistasio@gmail.com>
2019-11-01 23:12:43 -0500

    Creates action creator for new food advisor, adds it to reducer

--- src/reducers/manageFoodAdvice.js
+++ src/reducers/manageFoodAdvice.js
@@ -1,14 +1,14 @@
-function manageFoodAdvice(state = {resources: []}, action){
+function manageFoodAdvice(state = {foodAdvisors: []}, action){
   switch (action.type){
     case 'FETCH_LATEST_FOOD_ADVICE':
       return {
         foodAdvisors: action.payload
       }
 
-    // case 'ADD_FOOD_ADVICE':
-    //   return {
-    //     ...state, foodAdvisors: [...state.foodAdvisors, action.payload]
-    //   }
+    case 'ADD_FOOD_ADVISOR':
+      return {    
+        ...state, foodAdvisors: [...state.foodAdvisors, action.payload]
+      }
     
     default:
       return state

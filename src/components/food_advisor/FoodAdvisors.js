f7971a00834fac296e9ab2ac67433ebf7c1a6c3f
Christopher DiStasio <cadistasio@gmail.com>
2019-12-17 20:15:15 -0600

    Shows raw values from most recent daily food advice input

--- src/components/food_advisor/FoodAdvisors.js
+++ src/components/food_advisor/FoodAdvisors.js
@@ -2,28 +2,28 @@ import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import RowStyles from '../styles/RowStyles'
 import TextStyles from '../styles/TextStyles'
-
+ 
 const FoodAdvisors = (props) => {
-  return (
-    <RowStyles>
-      <TextStyles>
-
-        <Row className='left-justified-row'>
-          <Col>
-            { 
-              props.foodAdvisors && props.foodAdvisors.forEach((function(foodAdvisor) {
-                  if (foodAdvisor === foodAdvisor[length-1]) {
-                    return foodAdvisor
-                  }
-                })
-              )
-            } 
-          </Col>   
-        </Row>
-        
-      </TextStyles>
-    </RowStyles>
-  )
+ return (
+   <RowStyles>
+     <TextStyles>
+ 
+       <Row className='left-justified-row'>
+         <Col>
+           {
+             props.foodAdvisors && props.foodAdvisors.map((function(foodAdvisor) {
+                 if (foodAdvisor === props.foodAdvisors[props.foodAdvisors.length-1]) {
+                   return <li key={foodAdvisor.id}>{foodAdvisor.how_big_meals}{foodAdvisor.how_much_snacked}{foodAdvisor.how_healthy}</li>
+                 }
+               })
+             )
+           }
+         </Col>  
+       </Row>
+      
+     </TextStyles>
+   </RowStyles>
+ )
 }
-
+ 
 export default FoodAdvisors

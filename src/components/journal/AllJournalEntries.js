182f95620a23b19a9a7ad039026f3e0891eaaf2a
Christopher DiStasio <cadistasio@gmail.com>
2019-12-27 12:10:25 -0600

    Removes like button from, restores delete button to all journal entries page

--- src/components/journal/AllJournalEntries.js
+++ src/components/journal/AllJournalEntries.js
@@ -5,67 +5,46 @@ import { deleteJournalEntry } from '../../actions/deleteJournalEntry'
 import { Row, Col, Button} from 'react-bootstrap'
 import ButtonStyles from '../styles/ButtonStyles'
 import RowStyles from '../styles/RowStyles'
-import LikeButton from '../LikeButton'
 
 class AllJournalEntries extends React.Component {
   
-  constructor (props) {
-    super (props)
-    this.state = {
-      likeCount: 0
-    }  
-    this.handleLikeCount = this.handleLikeCount.bind(this)
+  handleDelete = (journalEntry) => {
+    this.props.deleteJournalEntry(journalEntry.id)
   }
 
-  // handleDelete = (journalEntry) => {
-  //   this.props.deleteJournalEntry(journalEntry.id)
-  // }
-
-  handleLikeCount() {
-    this.setState(state => ({
-      likeCount: state.likeCount + 1
-    }))
-  } 
-
   render(){
+    return (
+      <RowStyles>
+        <ButtonStyles>
 
-    return(
-    <RowStyles>
-      <ButtonStyles>
-
-        <Row className='left-justified-row'>
-          <Col>
-            { 
-              this.props.journalEntries && [...this.props.journalEntries].map(journalEntry => 
-                <div key={journalEntry.id}> 
-                  <NavLink 
-                    exact to={`/journals/${journalEntry.id}`}
-                    style={{color: 'white'}}
-                  >
-                    {journalEntry.content}
-                  </NavLink>
-                  <br></br><br></br>
-
-                  <LikeButton />
-
-                  {/* <br></br><br></br> */}
-                    {/* <Button 
-                      onClick={() => handleDelete(journalEntry)}
+          <Row className='left-justified-row'>
+            <Col>
+              { 
+                this.props.journalEntries && [...this.props.journalEntries].map(journalEntry => 
+                  <div key={journalEntry.id}> 
+                    <NavLink 
+                      exact to={`/journals/${journalEntry.id}`}
+                      style={{color: 'white'}}
+                    >
+                      {journalEntry.content}
+                    </NavLink>
+                    <br></br><br></br>
+                    <Button 
+                      onClick={() => this.handleDelete(journalEntry)}
                       className='btn-submit'
                     >
                       Delete Entry
-                    </Button> */}
+                    </Button>
+                    <br></br><br></br><br></br><br></br>
+                  </div>
+                )}
+            </Col>
+          </Row>
 
-                  <br></br><br></br><br></br>
-                </div>
-              )}
-          </Col>
-        </Row>
-
-      </ButtonStyles>
-    </RowStyles>                
-  )
-}
+        </ButtonStyles>
+      </RowStyles>                
+    )
+  }
 }
 
 export default connect(null, {deleteJournalEntry})(AllJournalEntries)

024a775f668dfd3165b9e9b0fba13414f84d34ae
Christopher DiStasio <cadistasio@gmail.com>
2019-12-26 19:39:38 -0600

    Removes redundancy of portion message in results; refactors for clarity

--- src/components/food_advisor/FoodAdvisors.js
+++ src/components/food_advisor/FoodAdvisors.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
 import RowStyles from '../styles/RowStyles'
 import TextStyles from '../styles/TextStyles'
  
@@ -15,38 +15,43 @@ function FoodAdvisors(props){
      <TextStyles>
  
        <Row className='centered-row-form'>
-           {
-             props.foodAdvisors && props.foodAdvisors.map((function(foodAdvisor) {
+            { 
+              props.foodAdvisors && props.foodAdvisors.map((function(foodAdvisor) {
+
+                const howBigMealsCond = (props.foodAdvisors[props.foodAdvisors.length-1].how_big_meals + 
+                  props.foodAdvisors[props.foodAdvisors.length-2].how_big_meals + 
+                  props.foodAdvisors[props.foodAdvisors.length-3].how_big_meals)/3 > 3
+              
+                const howMuchSnackedCond = (props.foodAdvisors[props.foodAdvisors.length-1].how_much_snacked + 
+                  props.foodAdvisors[props.foodAdvisors.length-2].how_much_snacked + 
+                  props.foodAdvisors[props.foodAdvisors.length-3].how_much_snacked)/3 > 3
+              
+                const howHealthyCond = (props.foodAdvisors[props.foodAdvisors.length-1].how_healthy + 
+                  props.foodAdvisors[props.foodAdvisors.length-2].how_healthy + 
+                  props.foodAdvisors[props.foodAdvisors.length-3].how_healthy)/3 > 3
 
                 if (foodAdvisor === props.foodAdvisors[props.foodAdvisors.length-1]) {
 
-                  if ((props.foodAdvisors[props.foodAdvisors.length-1].how_big_meals + 
-                      props.foodAdvisors[props.foodAdvisors.length-2].how_big_meals + 
-                      props.foodAdvisors[props.foodAdvisors.length-3].how_big_meals)/3 > 3
-                    ) {
-                      howBigMeals = "Have smaller portions at each meal."
+                  if (howBigMealsCond) {
+                    howBigMeals = "Have smaller portions at each meal."
                   } 
 
-                  if (((props.foodAdvisors[props.foodAdvisors.length-1].how_much_snacked + 
-                    props.foodAdvisors[props.foodAdvisors.length-2].how_much_snacked + 
-                    props.foodAdvisors[props.foodAdvisors.length-3].how_much_snacked)/3 > 3
-                  ) && howBigMeals === "Have smaller portions at each meal.") {
-                      howMuchSnacked = "Have smaller portions at each meal or have fewer snacks."
-                  } else if (((props.foodAdvisors[props.foodAdvisors.length-1].how_much_snacked + 
-                    props.foodAdvisors[props.foodAdvisors.length-2].how_much_snacked + 
-                    props.foodAdvisors[props.foodAdvisors.length-3].how_much_snacked)/3 > 3
-                  ) && howBigMeals !== "Have smaller portions at each meal.") {
-                      howMuchSnacked = "Have larger portions of healthy meals and fewer snacks."
+                  if (howMuchSnackedCond && howBigMeals === "Have smaller portions at each meal.") {
+                    howMuchSnacked = "Have smaller portions at each meal or have fewer snacks."
+                  } else if ((howMuchSnackedCond) && howBigMeals !== "Have smaller portions at each meal.") {
+                    howMuchSnacked = "Have larger portions of healthy meals and fewer snacks."
                   }
 
-                  if ((props.foodAdvisors[props.foodAdvisors.length-1].how_healthy + 
-                      props.foodAdvisors[props.foodAdvisors.length-2].how_healthy + 
-                      props.foodAdvisors[props.foodAdvisors.length-3].how_healthy)/3 > 3
-                    ) {
-                      howHealthy = "Be sure to make healthy choices when you have a meal or snack."
+                  if (howHealthyCond) {
+                    howHealthy = "Be sure to make healthy choices when you have a meal or snack."
                   }
+
                   if (howBigMeals === '' && howMuchSnacked === '' && howHealthy === ''){
                     noAdvice = "Nothing to pay special attention to now! Keep up the good work!"
+                  }
+                  
+                  if (howBigMeals === 'Have smaller portions at each meal.' && howMuchSnacked.includes('Have smaller portions at each meal')){
+                    howBigMeals = ''
                   }
                 } 
                 return (
@@ -60,9 +65,9 @@ function FoodAdvisors(props){
                     <h3>{noAdvice}</h3>
                   </div>
                 )
-               })
-             )
-           }
+              })
+            )
+          }
        </Row>
       
      </TextStyles>

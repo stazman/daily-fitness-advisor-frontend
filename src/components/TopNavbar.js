167b46c4857ef4dd1a34c8cf4fccdd448856dd8b
Christopher DiStasio <cadistasio@gmail.com>
2020-01-01 15:42:55 -0600

    Sets breakpoints for font sizes

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -2,27 +2,79 @@ import React from 'react'
 import { Navbar } from 'react-bootstrap'
 import { Nav } from 'react-bootstrap'
 import { NavDropdown } from 'react-bootstrap'
+import styled from 'styled-components'
 import NavbarStyles from '../components/styles/NavbarStyles'
+import { device } from '../components/styles/device'
+
+const MediaQueries = styled.div`
+
+  @media ${device.mobileS} {
+    a.navbar-brand.top {
+      font-size: 2.75rem;
+    }
+    a.dropdown-toggle.nav-link::after {
+      font-size: 2.5rem;
+    }
+  }
+
+  @media ${device.mobileM} {
+    a.navbar-brand.top {
+      font-size: 3.5rem;
+    }
+    a.dropdown-toggle.nav-link::after {
+      font-size: 3rem;
+    }
+  }
+
+  @media ${device.tabletL} {
+    a.navbar-brand.top {
+      font-size: 4rem;
+    }
+    a.dropdown-toggle.nav-link::after {
+      font-size: 3.5rem;
+    }
+  }
+
+  @media ${device.laptop} {
+    a.navbar-brand.top {
+      font-size: 4.5rem;
+    }
+    a.dropdown-toggle.nav-link::after {
+      font-size: 4rem;
+    }
+  }
+
+  @media ${device.desktop} {
+    a.navbar-brand.top {
+      font-size: 5rem;
+    }
+    a.dropdown-toggle.nav-link::after {
+      font-size: 4.5rem;
+    }
+  }
+`
 
 const TopNavbar = () => {
   return (
     <>
-      <NavbarStyles>
-        <Navbar expand='xl' className='nb-bg' fixed='top'>
-        <Navbar.Brand href='/' className='top'>DWLA</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        {/* <Navbar.Collapse id='basic-navbar-nav'> */}
-          <Nav className='mr-auto'>
-            <NavDropdown id='basic-nav-dropdown'>
-              <NavDropdown.Item href='/'>About DWLA </NavDropdown.Item>
-              <NavDropdown.Item href='/'>Contact DWLA</NavDropdown.Item>
-              <NavDropdown.Item href='/'>Settings</NavDropdown.Item>
-              <NavDropdown.Item href='/'>Login/Logout</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        {/* </Navbar.Collapse> */}
-        </Navbar>
-      </NavbarStyles>
+      <MediaQueries>
+        <NavbarStyles>
+          <Navbar expand='xl' className='nb-bg' fixed='top'>
+          <Navbar.Brand href='/' className='top'>DWLA</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          {/* <Navbar.Collapse id='basic-navbar-nav'> */}
+            <Nav className='mr-auto'>
+              <NavDropdown id='basic-nav-dropdown'>
+                <NavDropdown.Item href='/'>About DWLA </NavDropdown.Item>
+                <NavDropdown.Item href='/'>Contact DWLA</NavDropdown.Item>
+                <NavDropdown.Item href='/'>Settings</NavDropdown.Item>
+                <NavDropdown.Item href='/'>Login/Logout</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          {/* </Navbar.Collapse> */}
+          </Navbar>
+        </NavbarStyles>
+      </MediaQueries>
     </>
   )
 }

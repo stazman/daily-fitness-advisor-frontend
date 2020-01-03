cdfcec5f1bbe74cf2d30f41972f42f522691ce79
Christopher DiStasio <cadistasio@gmail.com>
2020-01-02 21:59:46 -0600

    Adds device sizes

--- src/components/styles/device.js
+++ src/components/styles/device.js
@@ -1,23 +1,29 @@
 const size = {
+  mobileXS: "<320px",
   mobileS: "320px",
   mobileM: "375px",
   mobileL: "410px",
   tabletS: "500px",
   tabletL: "768px",
   laptop: "1024px",
-  custPos: "1200px",
+  custLaptopPos1: "1200px",
+  custLaptopPos2: "1328px",
+  custLaptopPos3: "1600px",
   laptopL: "1440px",
   desktop: "2560px"
 };
 
 export const device = {
+  mobileXS: `(min-width: ${size.mobileXS})`,
   mobileS: `(min-width: ${size.mobileS})`,
   mobileM: `(min-width: ${size.mobileM})`,
   mobileL: `(min-width: ${size.mobileL})`,
   tabletS: `(min-width: ${size.tabletS})`,
   tabletL: `(min-width: ${size.tabletL})`,
   laptop: `(min-width: ${size.laptop})`,
-  custPos: `(min-width: ${size.custPos})`,
+  custLaptopPos1: `(min-width: ${size.custLaptopPos1})`,
+  custLaptopPos2: `(min-width: ${size.custLaptopPos2})`,
+  custLaptopPos3: `(min-width: ${size.custLaptopPos3})`,
   laptopL: `(min-width: ${size.laptopL})`,
   desktop: `(min-width: ${size.desktop})`
 };

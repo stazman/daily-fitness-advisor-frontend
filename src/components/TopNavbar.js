89620d332b157b49bfa916b468080a89de32b9b2
Christopher DiStasio <cadistasio@gmail.com>
2020-01-06 16:56:57 -0600

    Edits, further sets font sizes, spacing in top navbar breakpoints

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -8,116 +8,94 @@ import { device } from '../components/styles/device'
  
 const MediaQueries = styled.div`
 
-  @media ${device.mobileS} {
-    a.navbar-brand.top {
-      font-size: 2.3rem;
+@media ${device.mobileS} {
+    a.top.navbar-brand {
+      font-size: 3.4rem;
     }
     a.dropdown-toggle.nav-link::after {
-      font-size: 2.5rem;
-    }
-    a.dropdown-item {
-      font-size: 2rem;
+      font-size: 4.5rem;
     }
     div.mr-auto.navbar-nav {
       position: fixed;
-      top: 3em;
-      right: 4em;
+      top: 5em;
+      right: 5em;
+    }
+    a.dropdown-item {
+      font-size: 3.4rem;
     }
   }
     
   @media ${device.mobileM} {
     a.navbar-brand.top {
-      font-size: 2.6rem;
+      font-size: 3.6rem;
     }
     a.dropdown-toggle.nav-link::after {
-      font-size: 3rem;
-    }
-    a.dropdown-item {
-      font-size: 2.5rem;
+      font-size: 4.75rem;
     }
     div.mr-auto.navbar-nav {
       position: fixed;
-      top: 4em;
-      right: 5em;
-    }    
+      top: 5rem;
+      right: 3rem;
+    }
+    a.dropdown-item {
+      font-size: 3.6rem;
+    }
   }
   
   @media ${device.tabletS} {
-    a.dropdown-item {
-      font-size: 3rem;
+    a.navbar-brand.top {
+      font-size: 4rem;
+      padding-left: 2rem;
+    }
+    div.mr-auto.navbar-nav {
+      position: fixed;
+      top: 6.5rem;
+      right: 5rem;
+    }
   }
     
   @media ${device.tabletL} {
     a.navbar-brand.top {
-      font-size: 4rem;
+      font-size: 4.6rem;
+      padding-left: 3rem;
     }
     a.dropdown-toggle.nav-link::after {
-      font-size: 3.5rem;
+      font-size: 5.5rem;
     }
     a.dropdown-item {
       font-size: 3.5rem;
     }
     div.mr-auto.navbar-nav {
       position: fixed;
-      top: 4em;
-      right: 6em;
-    }      
+      top: 7rem;
+      right: 5rem;
+    }
   }
   
   @media ${device.laptop} {
+    a.top.navbar-brand {
+      padding-left: 3rem;
+      padding-right: 0;
+    }
+    nav a.top-link.nav-link {
+      display: inline;
+      padding-left: 1.4rem;
+      padding-right: 6.5rem;
+      font-size: 3.2rem;
+    }
     a.dropdown-toggle.nav-link::after {
       font-size: 4rem;
-      top: 2.75rem;
+      top: 3rem;
       right: 2rem;
     }
     div.mr-auto.navbar-nav {
       position: fixed;
-      top: 6em;
-      right: 7em;
-    }
-    nav a.top-link.nav-link {
-      display: inline-block;
+      top: 8rem;
+      right: 6rem;
     }
   }
     
   @media ${device.custLaptopPos1} {
-    a.navbar-brand.top {
-      padding-right: 6rem;
-    }
-    div.mr-auto.navbar-nav {
-      position: fixed;
-      top: 6rem;
-      right: 32rem;
-    }
-    nav a.top-link.nav-link {
-      font-size: 3.5rem;
-      padding-left: 5rem;
-      padding-right: 6rem;
-    }
-  }
-  
-  @media ${device.custLaptopPos2} {
-    a.navbar-brand.top {
-      font-size: 4.5rem;
-      padding-right: 7rem;
-    }
-    div.mr-auto.navbar-nav {
-      position: fixed;
-      top: 6rem;
-      right: 32rem;
-    }
-    nav a.top-link.nav-link {
-      font-size: 4rem;
-      padding-left: 6rem;
-      padding-right: 7rem;
-    }
-  }
-  
-  @media ${device.custLaptopPos3} {
-    a.navbar-brand.top {
-      font-size: 5rem;
-      padding-right: 10rem;
-    }
     div.mr-auto.navbar-nav {
       position: fixed;
       top: 6rem;
@@ -126,25 +104,103 @@ const MediaQueries = styled.div`
     nav a.top-link.nav-link {
       font-size: 3.5rem;
       padding-left: 7rem;
+      padding-right: 4rem;
+    }
+    a.dropdown-toggle.nav-link::after {
+      font-size: 5.2rem;
+      top: 3.2rem;
+      right: 4rem;
+    }
+    div.mr-auto.navbar-nav {
+      position: fixed;
+      top: 8rem;
+      right: 32rem;
+    }
+  }
+  
+  @media ${device.custLaptopPos2} {
+    a.navbar-brand.top {
+      font-size: 4.5rem;
+    }
+    nav a.top-link.nav-link {
+      font-size: 4rem;
+      padding-left: 6rem;
+      padding-right: 7rem;
+    }
+    div.mr-auto.navbar-nav {
+      position: fixed;
+      top: 8rem;
+      right: 32rem;
+    }
+  }
+  
+  @media ${device.laptopL} {
+    a.top.navbar-brand {
+      font-size: 5rem;
+      padding-left: 3.5rem;
+    }
+    nav a.top-link.nav-link {
+      font-size: 4.2rem;
+      padding-left: 8rem;
+      padding-right: 6rem;
+    }
+    a.dropdown-toggle.nav-link::after {
+      font-size: 5.6rem;
+      top: 4rem;
+      right: 4rem;
+    }
+  }
+
+  @media ${device.custLaptopPos3} {
+    a.navbar-brand.top {
+      font-size: 5rem;
+      padding-left: 6rem;
+    }
+    nav a.top-link.nav-link {
+      font-size: 4.8rem;
+      padding-left: 8rem;
       padding-right: 8rem;
+    }
+    div.sc-ifAKCX.iYsIqr {
+      margin-right: 0;
+    }
+    a.dropdown-toggle.nav-link::after {
+      font-size: 6rem;
+      top: 4rem;
+      right: 4rem;
+    }
+    div.mr-auto.navbar-nav {
+      position: fixed;
+      top: 8.5rem;
+      right: 36rem;
+    }
+    a.dropdown-item {
+      font-size: 4rem;
     }
   }
   
   @media ${device.desktop} {
     a.navbar-brand.top {
       font-size: 5rem;
-      padding-right: 12rem;
-    }
-    a.dropdown-toggle.nav-link::after {
-      font-size: 4.5rem;
-    }
-    a.dropdown-item {
-      font-size: 4rem;
+      padding-left: 7rem;
     }
     nav a.top-link.nav-link {
       font-size: 4.5rem;
-      padding-left: 18rem;
-      padding-right: 24rem;
+      padding-left: 22rem;
+      padding-right: 16rem;
+    }
+    a.dropdown-toggle.nav-link::after {
+      font-size: 6.5rem;
+      top: 3.8rem;
+      right: 5rem;
+    }
+    div.mr-auto.navbar-nav {
+      position: fixed;
+      top: 9rem;
+      right: 40rem;
+    }
+    a.dropdown-item {
+      font-size: 4.3rem;
     }
   }
 `
@@ -155,7 +211,7 @@ const TopNavbar = () => {
       <MediaQueries>
         <NavbarStyles>
           <Navbar expand='xl' className='nb-bg' fixed='top'>
-          <Navbar.Brand href='/' className='top'>DWLA</Navbar.Brand>
+          <Navbar.Brand href='/' className='top'>D</Navbar.Brand>
           <Nav.Link className='top-link'href="/tracker_home">Trackers</Nav.Link>
           <Nav.Link className='top-link'href="/journal_home">Journal</Nav.Link>
           <Nav.Link className='top-link'href="/resource_home">Resources</Nav.Link>

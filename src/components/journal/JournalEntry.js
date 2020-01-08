171e43bd9056d1191f3e00f2c7d48b3bcd0f9650
Christopher DiStasio <cadistasio@gmail.com>
2020-01-08 09:44:03 -0600

    Sets breakpoints in individual journal entry page

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,7 +1,36 @@
 import React from 'react'
 import {Redirect} from 'react-router-dom'
 import { Row, Col } from 'react-bootstrap'
+import styled from 'styled-components'
+import { device } from '../styles/device'
 import RowStyles from '../styles/RowStyles'
+import ColumnStyles from '../styles/ColumnStyles'
+import TextStyles from '../styles/ColumnStyles'
+
+const MediaQueries = styled.div`
+
+  @media ${device.mobileS} {
+    p {
+      font-size: 2.4rem;
+    }
+  }    
+  
+  @media ${device.tabletS} {
+    p {
+      font-size: 2.6rem;
+    }
+  }
+  
+  @media ${device.tabletL} {
+    p {
+      font-size: 3rem;
+  }  
+
+  @media ${device.laptop} {
+    p {
+      font-size: 3.2rem;
+  }  
+`
 
 const JournalEntry = (props) => {
 
@@ -10,14 +39,27 @@ const JournalEntry = (props) => {
   const journalEntry = reverseProps[props.match.params.id - 1]
 
   return (
-    <RowStyles>
-      <Row className='left-justified-row'>
-        <Col>
-          {journalEntry ? null : <Redirect to='/journals' />}
-          {journalEntry ? journalEntry.content : null}
-        </Col>
-      </Row>
-    </RowStyles>
+    <MediaQueries>
+      <RowStyles>
+        <ColumnStyles>
+          <TextStyles>
+            <Row className='left-justified-row'>
+              <Col className='bottom-visible' 
+                style={{paddingTop: '10rem', 
+                  paddingBottom: '2rem', 
+                  marginLeft: '2rem', 
+                  marginRight: '2.5rem'
+                }}>
+                <p>
+                  {journalEntry ? null : <Redirect to='/journals' />}
+                  {journalEntry ? journalEntry.content : null}
+                </p>
+              </Col>
+            </Row>
+          </TextStyles>
+        </ColumnStyles>
+      </RowStyles>
+    </MediaQueries>
   )
 }
 

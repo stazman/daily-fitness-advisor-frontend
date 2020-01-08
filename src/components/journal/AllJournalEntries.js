2c1d14e89b96f5a6c6f39c93da31312e85de8d25
Christopher DiStasio <cadistasio@gmail.com>
2020-01-08 09:34:25 -0600

    Sets breakpoints in all journal entries page

--- src/components/journal/AllJournalEntries.js
+++ src/components/journal/AllJournalEntries.js
@@ -3,9 +3,51 @@ import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { deleteJournalEntry } from '../../actions/deleteJournalEntry'
 import { Row, Col, Button} from 'react-bootstrap'
+import styled from 'styled-components'
+import { device } from '../styles/device'
 import RowStyles from '../styles/RowStyles'
 import ColumnStyles from '../styles/ColumnStyles'
 import ButtonStyles from '../styles/ButtonStyles'
+import TextStyles from '../styles/TextStyles'
+
+const MediaQueries = styled.div`
+
+  @media ${device.mobileS} {
+    a {
+      font-size: 2.4rem;
+    }
+    button.btn-submit {
+      font-size: 2.2rem;
+    }
+  }    
+  
+  @media ${device.tabletS} {
+    a {
+      font-size: 2.6rem;
+    }
+    button.btn-submit {
+      font-size: 2.4rem;
+    }
+  }
+  
+  @media ${device.tabletL} {
+    a {
+      font-size: 3rem;
+    }
+    button.btn-submit {
+      font-size: 2.6rem;
+    }
+  }  
+
+  @media ${device.laptop} {
+    a {
+      font-size: 3.2rem;
+    }
+    button.btn-submit {
+      font-size: 2.8rem;
+    }
+  }  
+`
 
 class AllJournalEntries extends React.Component {
   
@@ -15,37 +57,41 @@ class AllJournalEntries extends React.Component {
 
   render(){
     return (
-      <RowStyles>
-        <ColumnStyles>
-          <ButtonStyles>
+      <MediaQueries>
+        <RowStyles>
+          <ColumnStyles>
+            <ButtonStyles>
+              <TextStyles>
 
-            <Row className='left-justified-row'>
-              <Col className='bottom-visible' style={{paddingTop: '4rem'}}>
-                { 
-                  this.props.journalEntries && [...this.props.journalEntries].map(journalEntry => 
-                    <div key={journalEntry.id}> 
-                      <NavLink 
-                        exact to={`/journals/${journalEntry.id}`}
-                        style={{color: 'white'}}
-                      >
-                        {journalEntry.content}
-                      </NavLink>
-                      <br></br><br></br>
-                      <Button 
-                        onClick={() => this.handleDelete(journalEntry)}
-                        className='btn-submit'
-                      >
-                        Delete
-                      </Button>
-                      <br></br><br></br><br></br>
-                    </div>
-                  )}
-              </Col>
-            </Row>
+                <Row className='left-justified-row'>
+                  <Col className='bottom-visible' style={{paddingTop: '8rem'}}>
+                    { 
+                      this.props.journalEntries && [...this.props.journalEntries].map(journalEntry => 
+                        <div key={journalEntry.id}> 
+                          <NavLink 
+                            exact to={`/journals/${journalEntry.id}`}
+                          >
+                            {journalEntry.content}
+                          </NavLink>
+                          <br></br><br></br>
+                          <Button 
+                            type='submit'
+                            className='btn-submit'
+                            onClick={() => this.handleDelete(journalEntry)}
+                          >
+                            Delete
+                          </Button>
+                          <br></br><br></br><br></br><br></br><br></br>
+                        </div>
+                      )}
+                  </Col>
+                </Row>
 
-          </ButtonStyles>
-        </ColumnStyles>
-      </RowStyles>                
+              </TextStyles>
+            </ButtonStyles>
+          </ColumnStyles>
+        </RowStyles>
+      </MediaQueries>                
     )
   }
 }

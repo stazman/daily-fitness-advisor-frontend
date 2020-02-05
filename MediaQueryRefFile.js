502c6e49db8d84bba299e6681b2c34fe6d289541
Christopher DiStasio <cadistasio@gmail.com>
2020-02-05 17:27:02 -0600

    Adds media query reference file

---
+++ MediaQueryRefFile.js
@@ -0,0 +1,174 @@
+import styled from 'styled-components'
+
+const MediaQueries = styled.div`
+
+
+  @media ${device.mobileS} {
+    h5 {
+      font-size: 3rem;
+    }
+    label {
+      font-size: 1rem;
+    }
+  }
+
+  @media ${device.mobileM} {
+    h5 {
+      font-size: 3rem;
+    }
+    label {
+      font-size: 1rem;
+    }
+
+  @media ${device.mobileL} {
+    h5 {
+      font-size: 3rem;
+    }
+    label {
+      font-size: 1rem;
+    }
+  }
+
+  @media ${device.tabletS} {
+    h5 {
+      font-size: 3rem;
+    }
+    label {
+      font-size: 1rem;
+    }
+  }
+
+  @media ${device.tabletL} {
+    h5 {
+      font-size: 3rem;
+    }
+    label {
+      font-size: 1rem;
+    }
+  }
+
+  @media ${device.laptop} {
+    h5 {
+      font-size: 3rem;
+    }
+    label {
+      font-size: 1rem;
+    }
+  }
+
+  @media ${device.laptopL} {
+    h5 {
+      font-size: 3rem;
+    }
+    label {
+      font-size: 1rem;
+    }
+  }
+
+  @media ${device.desktop} {
+    h5 {
+      font-size: 3rem;
+    }
+    label {
+      font-size: 1rem;
+    }
+  }
+
+
+
+
+
+
+..................................
+
+
+  @media ${device.mobileS} {
+    h4 {
+      font-size: 2rem;
+    }
+    a.btn-main {
+      font-size: 1.6rem;
+    }
+    div.col-lg-12.button-col {
+      padding-top: 4rem;
+      padding-bottom: 4rem;
+    }
+  }
+
+  @media ${device.mobileM} {
+    h4 {
+      font-size: 2.3rem;
+    }
+    a.btn-main {
+      font-size: 2rem;
+    }
+    div.col-lg-12.button-col {
+      padding-top: 5rem;
+      padding-bottom: 5rem;
+    }
+  }
+
+  @media ${device.mobileL} {
+    h4 {
+      font-size: 2.4rem;
+  }
+
+  @media ${device.tabletS} {
+    h4 {
+      font-size: 2.6rem;
+    }
+    a.btn-main {
+      font-size: 2.6rem;
+    }
+  }
+
+  @media ${device.tabletL} {
+    h4 {
+      font-size: 3rem;
+    }
+    a.btn-main {
+      font-size: 2.8rem;
+    }
+    div.col-lg-12.button-col {
+      padding-top: 6rem;
+      padding-bottom: 6rem;
+    }
+  }
+
+  @media ${device.laptop} {
+    h4 {
+      font-size: 4rem;
+    }
+    a.btn-main {
+      font-size: 3.6rem;
+    }
+    div.col-lg-12.button-col {
+      padding-bottom: 7rem;
+    }
+  }
+
+  @media ${device.laptopL} {
+    div.col-lg-12.button-col {
+      padding-top: 7rem;
+      padding-bottom: 8rem;
+  }
+
+  @media ${device.desktop} {
+    h4 {
+      font-size: 5.5rem;
+    }
+    a.btn-main {
+      font-size: 4rem;
+    }
+    div.col-lg-12.button-col {
+      padding-top: 8.5rem;
+      padding-bottom: 10rem;
+    }
+    div.sc-bxivhb.icWcwL {
+      width: 120%;
+      margin-left: -16rem;
+    } 
+  }
+`
+
+export default MediaQueries
